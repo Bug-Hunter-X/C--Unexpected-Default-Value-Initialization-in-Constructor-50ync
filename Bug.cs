6af6b35b851b6562fe65f98a18d86ee13c680b7a
BugHunter-X <askzenai@gmail.com>
2025-02-10 03:56:04 +0100

    Add Bug.cs file

---
+++ Bug.cs
@@ -0,0 +1,16 @@
+public class MyClass
+{
+    public int MyProperty { get; set; }
+
+    public MyClass()
+    {
+        // Initialize MyProperty to a default value
+        MyProperty = 0; 
+    }
+
+    public void MyMethod()
+    {
+        // Access and modify MyProperty
+        MyProperty++;
+    }
+}
